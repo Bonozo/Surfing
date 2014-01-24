95bdc9c7f9c94d8261c1118fb56fddb890429479
Aharon Smbatyan <aharon91@mail.ru>
2014-01-24 05:40:05 +0400

    Added water spray effects to few obstacles.

---
+++ Surfing Tsunami/Assets/Sources/Game Play/FacedParticleFixer.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FacedParticleFixer : MonoBehaviour {
+
+	float dist;
+	void Start()
+	{
+		dist = transform.localPosition.z;
+	}
+
+	void Update()
+	{
+		var lp = transform.localPosition;
+		bool cnd = Mathf.Abs (transform.parent.localRotation.eulerAngles.y) < 0.1f;
+		lp.z = cnd?dist:-dist;
+		transform.localPosition = lp;
+	}
+}
