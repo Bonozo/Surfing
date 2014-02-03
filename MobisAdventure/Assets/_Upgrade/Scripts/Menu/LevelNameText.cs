58ed731c69c7a385bd71edeb17f05dbacd9090b9
Aharon Smbatyan <aharon91@mail.ru>
2014-02-03 18:24:15 +0400

    Mobi: Added current level info to the levels menu.

---
+++ MobisAdventure/Assets/_Upgrade/Scripts/Menu/LevelNameText.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelNameText : MonoBehaviour {
+
+	public string levelName;
+	public string id;
+
+	void OnEnable()
+	{
+		
+		var idd = "level_" + id;
+		var currentLevel = PlayerPrefs.GetInt (idd, 1);
+
+		transform.FindChild ("Name").GetComponent<UILabel> ().text = levelName + " (level: " + currentLevel + " )";
+	}
+}
