eb36b53b07e916d61774674adba950be93187719
Aharon Smbatyan <aharon91@mail.ru>
2014-02-13 01:44:26 +0400

    ZIP: Added back button to each level.

---
+++ ZIP's Big Adventure/Assets/Scripts/Menus/ButtonBack.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonBack : MonoBehaviour {
+
+	bool started = false;
+	void OnClick()
+	{
+		if(!started)
+		{
+			started = true;
+			StartCoroutine (EndGame ());
+		}
+	}
+	
+	IEnumerator EndGame()
+	{
+		Fireworks[] fw = GameObject.FindObjectsOfType<Fireworks> ();
+		foreach(var ff in fw) Destroy(ff.gameObject);
+		
+		yield return new WaitForSeconds (0.25f);
+		Loader.sceneName = "title";
+		Application.LoadLevel ("loader");
+	}
+}
