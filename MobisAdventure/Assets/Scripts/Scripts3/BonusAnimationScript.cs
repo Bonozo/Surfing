3d56add7cd59aa6112bfdac8d0476ce395a9c7f4
Aharon Smbatyan <aharon91@mail.ru>
2014-02-16 18:49:34 +0400

    Mobi: Reduced bonus text speed and added 0.25sec delay.

--- MobisAdventure/Assets/Scripts/Scripts3/BonusAnimationScript.cs
+++ MobisAdventure/Assets/Scripts/Scripts3/BonusAnimationScript.cs
@@ -2,17 +2,27 @@ using UnityEngine;
 using System.Collections;
 
 public class BonusAnimationScript : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
 	
+	void OnEnable()
+	{
+		StartCoroutine (ShowBonus ());
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
+	IEnumerator ShowBonus()
+	{
+		yield return new WaitForEndOfFrame();
+		animation ["BonusText"].speed = 0.000001f;
+		yield return new WaitForSeconds(0.25f);
+		animation ["BonusText"].speed = 0.75f;
+		while(animation.isPlaying)
+			yield return new WaitForEndOfFrame();
+		gameObject.SetActive (false);
+	}
+
+	/*void Update () 
+	{
 		if( !this.animation.isPlaying )
 			this.gameObject.SetActive( false );
 	
-	}
+	}*/
 }
