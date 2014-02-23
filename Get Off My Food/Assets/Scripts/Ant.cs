abae6e7d4e4c1661d1f62078bed7f04484d56006
Aharon Smbatyan <aharon91@mail.ru>
2014-02-23 22:10:24 +0400

    Ants: Added 3 ants textures.

--- Get Off My Food/Assets/Scripts/Ant.cs
+++ Get Off My Food/Assets/Scripts/Ant.cs
@@ -8,20 +8,23 @@ public class Ant : MonoBehaviour {
 
 	void Start()
 	{
-		Debug.Log ("new ant");
 		float dist = 800f;
 		float alpha = Random.Range (0f, 360f);
-		float x = dist * Mathf.Sin (alpha);
-		float y = dist * Mathf.Cos (alpha);
+		float x = dist * Mathf.Cos (alpha * Mathf.PI / 180f);
+		float y = dist * Mathf.Sin (alpha * Mathf.PI / 180f);
+		Debug.Log ("x:" + x + ",y: " + y);
 
 		transform.parent = LevelInfo.Instance.transform;
 		transform.localScale = new Vector3 (1f, 1f, 1f);
 		transform.localPosition = new Vector3 (x, y, 0f);
+		transform.localRotation = Quaternion.Euler (0f, 0f, alpha);
 
 		int wave = LevelInfo.Instance.CurrentWave;
 		float minSpeed = 75f + 5f * wave;
 		float maxSpeed = 125f + 10f * wave;
 		speed = Random.Range (minSpeed, maxSpeed);
+
+		GetComponentInChildren<UISprite> ().spriteName = "ant" + Random.Range (1, 4);
 	}
 
 	void Update()
@@ -46,10 +49,15 @@ public class Ant : MonoBehaviour {
 	{
 		collider.enabled = false;
 		LevelInfo.Instance.AddAnt ();
-		TweenScale.Begin (transform.GetChild (0).gameObject, 0.1f, transform.localScale*1.15f);
-		yield return new WaitForSeconds (0.1f);
-		TweenScale.Begin (transform.GetChild (0).gameObject, 0.5f, Vector3.zero);
+		GetComponentInChildren<UISprite> ().color = new Color (0.5f, 0.5f, 0.5f, 1f);
+		yield return new WaitForSeconds (1f);
+		TweenAlpha.Begin (transform.GetChild (0).gameObject, 1f, 0f);
 		Destroy (this.gameObject, 1f);
+
+		//TweenScale.Begin (transform.GetChild (0).gameObject, 0.1f, transform.localScale*1.15f);
+		//yield return new WaitForSeconds (0.1f);
+		//TweenScale.Begin (transform.GetChild (0).gameObject, 0.5f, Vector3.zero);
+
 
 	}
 
