e067131a0d05cfc0feba0ddb6e028cc84fa28274
Aharon Smbatyan <aharon91@mail.ru>
2014-03-02 22:46:40 +0400

    ZIP: Numbers/Kindergarten/Substraction(8)

---
+++ ZIP's Big Adventure/Assets/Scripts/Editor/EndItemListEditor.cs
@@ -0,0 +1,17 @@
+﻿using UnityEditor;
+using UnityEngine;
+using System.Collections;
+
+
+[CustomEditor(typeof(EndItemList))]
+
+class EndItemListEditor : Editor {
+	
+	public override void OnInspectorGUI() {
+		
+		base.OnInspectorGUI();
+		if(GUILayout.Button("Add childs"))
+			((EndItemList)target).EditorAddChilds();
+	}
+	
+}
---
+++ ZIP's Big Adventure/Assets/Scripts/Procedures/EndItemList.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EndItemList : EndItem {
+
+	public EndItem[] list;
+
+	public override void Reset ()
+	{
+		foreach(var item in list) item.Reset();
+	}
+	
+	public override void Work ()
+	{
+		foreach(var item in list) item.Work();
+	}
+
+	#region Editor
+
+	public void EditorAddChilds()
+	{
+		int count = 0;
+		foreach(Transform item in transform)
+			if(item.GetComponent<EndItem>()!=null)
+				count++;
+		list = new EndItem[count];
+		count = 0;
+		foreach(Transform item in transform)
+			if(item.GetComponent<EndItem>()!=null)
+				list[count++]=item.GetComponent<EndItem>();;
+	}
+
+	#endregion
+}
