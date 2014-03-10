28b1f9d76543380eceed2d55ad8d7be5a58ee7c5
Aharon Smbatyan <aharon91@mail.ru>
2014-03-10 18:27:35 +0400

    Mobi: Showing Game Services Sprite on Android and Game Center Sprite on iPhone.

---
+++ MobisAdventure/Assets/_Upgrade/Scripts/Death,Game/ChooseSprite.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChooseSprite : MonoBehaviour {
+
+	public string androidSprite;
+	public string iPhoneSprite;
+
+	void Awake()
+	{
+		#if UNITY_ANDROID
+		GetComponent<UISprite>().spriteName = androidSprite;
+		#endif
+		#if UNITY_IPHONE
+		GetComponent<UISprite>().spriteName = iPhoneSprite;
+		#endif
+	}
+}
