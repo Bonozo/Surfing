0ab76910bc3114a5d2935cfb9ada21e1f55b4555
Sinan Özeray <sinanozeray@gmail.com>
2014-03-10 02:44:13 +0200

    Mobi: iOS GameCenter Integration
    Integrated GameCenter leaderboard in "iOS" preprocessor just below the
    GPGS code. It works exactly like the Google Play Game Services
    leaderboard.

--- MobisAdventure/Assets/_Upgrade/Scripts/Integration/GameServicesSubmit.cs
+++ MobisAdventure/Assets/_Upgrade/Scripts/Integration/GameServicesSubmit.cs
@@ -1,7 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
-using GooglePlayGames;
 using UnityEngine.SocialPlatforms;
+
+#if UNITY_IPHONE
+using UnityEngine.SocialPlatforms.GameCenter;
+#endif
+
+#if UNITY_ANDROID
+using GooglePlayGames;
+#endif
 
 public class GameServicesSubmit : MonoBehaviour {
 
@@ -23,5 +30,47 @@ public class GameServicesSubmit : MonoBehaviour {
 		});
 	}
 
+	#endif
+
+	#if UNITY_IPHONE
+	
+		void OnClick()
+		{
+			DoLeaderboard ();
+		}
+
+		void DoLeaderboard () {
+		
+			Social.localUser.Authenticate (success => {
+				
+				if (success) {
+
+					int score = DeathScreen.Instance.LastDistanceTravelled;
+
+					Debug.Log ("Authentication successful");
+					string userInfo = "Username: " + Social.localUser.userName + 
+						"\nUser ID: " + Social.localUser.id + 
+							"\nIsUnderage: " + Social.localUser.underage;
+					Debug.Log (userInfo);
+					
+					Social.CreateLeaderboard();
+
+						Social.CreateLeaderboard().id = "mobisRunLeaderboard";
+						ReportScore(score, "mobisRunLeaderboard");
+
+					Social.ShowLeaderboardUI();
+				}
+				else
+					Debug.Log ("Authentication failed");
+			} );
+		}
+	
+	void ReportScore (long score, string leaderboardID) {
+		Debug.Log ("Reporting score " + score + " on leaderboard " + leaderboardID);
+		Social.ReportScore (score, leaderboardID, success => {
+			Debug.Log(success ? "Reported score successfully" : "Failed to report score");
+		});
+	}
+	
 	#endif
 }
