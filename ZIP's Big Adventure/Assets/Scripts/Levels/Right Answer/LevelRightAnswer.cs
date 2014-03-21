25ea4b7799714c687c233164d1da8986aeedf0be
Aharon Smbatyan <aharon91@mail.ru>
2014-03-21 05:48:11 +0400

    ZIP: Shapes/Kindergarten/Compare(16)

--- ZIP's Big Adventure/Assets/Scripts/Levels/Right Answer/LevelRightAnswer.cs
+++ ZIP's Big Adventure/Assets/Scripts/Levels/Right Answer/LevelRightAnswer.cs
@@ -44,103 +44,52 @@ public class LevelRightAnswer : ZIPLevel {
 	#region Editor
 
 	//public string answer1, answer2, answer3;
-	public int rightanswrindex;
-	public int howmany;
-	public UISprite ans1, ans2, rightans;
-	public UISprite[] hms;
-	public EndItemList shapes;
-	public GameObject shapeX;
+	public int correctAnswerIndex;
+	public UISprite appears, appearInx;
 	
 	public void Initialize()
 	{
-		if(transform.FindChild ("Game").FindChild ("_begin") != null)
-			DestroyImmediate(transform.FindChild ("Game").FindChild ("_begin").gameObject);
-		//ans1.GetComponent<BoxCollider> ().size = new Vector3 (2f, 1.5f, 1f);
-		//ans2.GetComponent<BoxCollider> ().size = new Vector3 (2f, 1.5f, 1f);
-		//rightans.GetComponent<BoxCollider> ().size = new Vector3 (2f, 1.5f, 1f);
-
+		for(int i=0;i<4;i++)
+			buttons[i].GetComponent<BoxCollider>().size = new Vector3(1f,1f,1f);
 		transform.localPosition = new Vector3 (0, 0, 0);
 		transform.localScale = new Vector3 (1, 1, 1);
-
-		Vector3 cc1 = new Vector3 (0, -390, 0);
-		Vector3 cc2 = new Vector3 (175, -390, 0);
-		Vector3 cc3 = new Vector3 (350, -390, 0);
-
-		//int idd = 0;
-		if(rightanswrindex==1)
-		{
-			//idd = answer1;
-			//rightans.spriteName = ""+answer1;
-			rightans.transform.localPosition = cc1;
-			//ans1.spriteName = ""+answer2;
-			ans1.transform.localPosition = cc2;
-			//ans2.spriteName= ""+answer3;
-			ans2.transform.localPosition = cc3;
-		}
-		if(rightanswrindex==2)
-		{
-			//idd = answer2;
-			//rightans.spriteName = ""+answer2;
-			rightans.transform.localPosition = cc2;
-			//ans1.spriteName = ""+answer1;
-			ans1.transform.localPosition = cc1;
-			//ans2.spriteName= ""+answer3;
-			ans2.transform.localPosition = cc3;
-		}
-		if(rightanswrindex==3)
-		{
-			//idd = answer3;
-			//rightans.spriteName = ""+answer3;
-			rightans.transform.localPosition = cc3;
-			//ans1.spriteName = ""+answer1;
-			ans1.transform.localPosition = cc1;
-			//ans2.spriteName= ""+answer2;
-			ans2.transform.localPosition = cc2;
-		}
-
-		proc (ans1);
-		proc (ans2);
-		proc (rightans);
-
 		gameBlock.level [0] = this;
 
-		foreach (var v in hms)
+		correctAnswer = buttons [correctAnswerIndex - 1];
+		endItems = new EndItem[5];
+		appears.color = new Color (1f, 1f, 1f, 1f-appears.color.a);
+		appearInx.gameObject.SetActive (!appearInx.gameObject.activeSelf);
+
+		for(int i=0;i<4;i++)
 		{
-			v.spriteName = "" + howmany;
-			v.MakePixelPerfect();
-		}
+			if(buttons[i].GetComponent<EndItemColor>() != null)
+				DestroyImmediate(buttons[i].GetComponent<EndItemColor>());
+			if(buttons[i].GetComponent<EndItemMoveTo>() != null)
+				DestroyImmediate(buttons[i].GetComponent<EndItemMoveTo>());
+			if(buttons[i].GetComponent<EndItemMultipleEndItem>() != null)
+				DestroyImmediate(buttons[i].GetComponent<EndItemMultipleEndItem>());
+			if(buttons[i].GetComponent<EndItemChangeTexture>() != null)
+				DestroyImmediate(buttons[i].GetComponent<EndItemChangeTexture>());
 
+			buttons[i].GetComponent<UISprite>().MakePixelPerfect();
 
-		foreach(Transform item in shapes.transform)
-		{
-			if(item.GetComponent<EndItemColor>() != null)
-				DestroyImmediate(item.GetComponent<EndItemColor>());
-			if(item.GetComponent<EndItemChangeTexture>() != null)
-				DestroyImmediate(item.GetComponent<EndItemChangeTexture>());
-
-			if(item.name == "shape" && item.GetComponent<UISprite>().spriteName != rightans.spriteName)
+			if(i == correctAnswerIndex-1)
 			{
-				item.gameObject.AddComponent<EndItemColor>();
-				item.gameObject.GetComponent<EndItemColor>().to = new Color(1f,1f,1f,0f);
-				item.gameObject.GetComponent<EndItemColor>().duration = 1f;
-				item.gameObject.GetComponent<EndItemColor>().delay = 0.0f;
+				buttons[i].gameObject.AddComponent<EndItemMoveTo>();
+				buttons[i].GetComponent<EndItemMoveTo>().defaultUp = false;
+				buttons[i].GetComponent<EndItemMoveTo>().duration = 1.5f;
+				buttons[i].GetComponent<EndItemMoveTo>().to = new Vector3(0f,200f,0f);
 			}
-
-			if(item.name == "shapeX")
+			else
 			{
-				item.gameObject.AddComponent<EndItemColor>();
-				item.gameObject.GetComponent<EndItemColor>().to = new Color(1f,1f,1f,1f);
-				item.gameObject.GetComponent<EndItemColor>().duration = 1f;
-				item.gameObject.GetComponent<EndItemColor>().delay = 0.0f;
-				item.GetComponent<UISprite>().color = new Color(1f,1f,1f,1f- item.GetComponent<UISprite>().color.a);
+				buttons[i].gameObject.AddComponent<EndItemColor>();
+				buttons[i].GetComponent<EndItemColor>().to = new Color(1f,1f,1f,0f);
+					buttons[i].GetComponent<EndItemColor>().duration = 0.75f;
 			}
+			endItems[i] = buttons[i].GetComponent<EndItem>();
 		}
-		shapes.EditorAddChilds ();
+		endItems [4] = appears.GetComponent<EndItem> ();
 
-		shapeX.gameObject.GetComponent<EndItemColor>().to = new Color(1f,1f,1f,1f);
-		shapeX.gameObject.GetComponent<EndItemColor>().duration = 1f;
-		shapeX.gameObject.GetComponent<EndItemColor>().delay = 0.0f;
-		shapeX.GetComponent<UISprite>().color = new Color(1f,1f,1f,1f- shapeX.GetComponent<UISprite>().color.a);
 	}
 
 	void proc(UISprite sprite)
