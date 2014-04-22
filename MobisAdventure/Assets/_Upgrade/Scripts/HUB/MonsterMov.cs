fffd5dd48cebefd8753ce61dca92b62192617633
Aharon Smbatyan <aharon91@mail.ru>
2014-04-22 05:12:08 +0400

    Mobi: Added Yeti (not complete)

---
+++ MobisAdventure/Assets/_Upgrade/Scripts/HUB/MonsterMov.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MonsterMov : MonoBehaviour {
+	// Used only to find the monster in hierarchy
+	// Messing Up the Project
+}
---
+++ MobisAdventure/Assets/_Upgrade/Scripts/HUB/Yeti.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Yeti : MonoBehaviour {
+
+	public float scal = 0.1f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var pos = PlayerController.Instance.transform.position;
+		pos.x -= PlayerController.Instance.monsterDistance * scal;
+		//pos.x = Camera.main.ViewportToWorldPoint (new Vector3 (0f, pos.y, pos.z)).x;
+
+		float newHeight;
+		Path.m_path.GetHeight(transform.position, out newHeight);
+		pos.y = newHeight;
+
+		transform.position = pos;
+	}
+}
