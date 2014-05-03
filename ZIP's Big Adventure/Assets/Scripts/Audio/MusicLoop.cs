3061503e6cb95fce223251e008956d1918e2ea33
Aharon Smbatyan <aharon91@mail.ru>
2014-05-04 01:56:13 +0400

    ZIP: Trying not to play the music loop before a scene loading completes.

---
+++ ZIP's Big Adventure/Assets/Scripts/Audio/MusicLoop.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicLoop : MonoBehaviour {
+
+	IEnumerator Start()
+	{
+		yield return new WaitForEndOfFrame();
+		yield return new WaitForEndOfFrame();
+		yield return new WaitForEndOfFrame();
+		audio.Play ();
+	}
+
+	#region Static Instace
+	
+	//Multithreaded Safe Singleton Pattern
+	// URL: http://msdn.microsoft.com/en-us/library/ms998558.aspx
+	private static readonly object _syncRoot = new Object();
+	private static volatile MusicLoop _staticInstance;
+	public static MusicLoop Instance {
+		get {
+			if (_staticInstance == null) {				
+				lock (_syncRoot) {
+					_staticInstance = FindObjectOfType (typeof(MusicLoop)) as MusicLoop;
+					if (_staticInstance == null) {
+						Debug.LogError("The MusicLoop instance was unable to be found, if this error persists please contact support.");						
+					}
+				}
+			}
+			return _staticInstance;
+		}
+	}
+	
+	#endregion
+}
