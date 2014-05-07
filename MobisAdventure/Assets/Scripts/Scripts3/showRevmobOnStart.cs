4469c611be81deafb3897621ae627a9c902f08bc
Aharon Smbatyan <aharon91@mail.ru>
2014-05-07 19:04:56 +0400

    Mobi: Fixed Main menu gui resolutions for iPad aspect rations.

--- MobisAdventure/Assets/Scripts/Scripts3/showRevmobOnStart.cs
+++ MobisAdventure/Assets/Scripts/Scripts3/showRevmobOnStart.cs
@@ -2,15 +2,12 @@ using UnityEngine;
 using System.Collections;
 
 public class showRevmobOnStart : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
+	
+	void Start () 
+	{
+		#if !UNITY_EDITOR
 		if(PlayerPrefs.GetInt("RevmobStatus",0) == 0)
 			GameObject.Find("RevmobManager").SendMessage("showRevmobFullScreen",SendMessageOptions.DontRequireReceiver);
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+		#endif
 	}
 }
---
+++ MobisAdventure/Assets/_Upgrade/Scripts/Other/FixIpadHeight.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FixIpadHeight : MonoBehaviour {
+
+	void Awake()
+	{
+		float aspect = (float)Screen.width / (float)Screen.height;
+		if( aspect < 1.34f )
+			this.GetComponent<UIRoot>().manualHeight = 1200;
+	}
+}
