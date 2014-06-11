d0e7047a4fe8cf85087f336a461b7db88e084f75
Aharon Smbatyan <aharon91@mail.ru>
2014-06-11 15:51:35 +0400

    ZIP: Removed 2 atlases. (Letters/First/Punctuation).

--- MobisAdventure/Assets/_Upgrade/Scripts/Integration/IAB/StorePurchase.cs
+++ MobisAdventure/Assets/_Upgrade/Scripts/Integration/IAB/StorePurchase.cs
@@ -3,11 +3,22 @@ using System.Collections;
 
 public class StorePurchase : MonoBehaviour {
 
+	public static bool debug = true;
+
+	public int debugCoinsAdd = 0; // used in degug
 	public string iapMethod;
 
 	void OnClick()
 	{
-		StartCoroutine (MakePurchase ());
+		if(debug){
+			int coins = PlayerPrefs.GetInt("pp_coins");
+			coins += debugCoinsAdd;
+			PlayerPrefs.SetInt("pp_coins",coins);
+			PlayerPrefs.Save();
+		}
+		else{
+			StartCoroutine (MakePurchase ());
+		}
 	}
 
 	IEnumerator MakePurchase()
---
+++ ZIP's Big Adventure/Assets/Scripts/End Items/EndItemChangeText.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[AddComponentMenu("EndItems/Change Text")]
+public class EndItemChangeText : EndItem {
+
+	public string newText;
+	public float duration;
+	public float delay;
+	
+	private bool saved=false;
+	private UILabel label;
+	private string oldText;
+	private Vector3 saveScale;
+
+	public override void Reset ()
+	{
+		iTween.Stop (gameObject, false);
+		StopAllCoroutines ();
+		if(!saved)
+		{
+			label = GetComponent<UILabel>();
+			oldText = label.text;
+			saveScale = transform.localScale;
+			saved=true;
+		}
+		label.text = oldText;
+		transform.localScale = saveScale;
+	}
+
+	public override void Work ()
+	{
+		Reset ();
+		StartCoroutine ("Working");
+	}
+
+	IEnumerator Working()
+	{
+		yield return new WaitForSeconds(delay);
+		iTween.ScaleTo(gameObject,iTween.Hash("scale",new Vector3(1f,1f,1f) ,"time",duration,"islocal",true));
+		yield return new WaitForSeconds(duration+0.2f);
+		label.text = newText;
+		iTween.ScaleTo(gameObject,iTween.Hash("scale",saveScale ,"time",duration,"islocal",true));
+		yield return new WaitForSeconds(duration);
+	}
+
+	#if UNITY_EDITOR
+	void Update()
+	{
+		if(Input.GetKeyDown(KeyCode.Space))
+			Work();
+	}
+	#endif
+}
