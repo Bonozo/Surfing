c63b4fa038c157581cfce339b5098e1a9db5c8c8
Aharon Smbatyan <aharon91@mail.ru>
2014-06-11 16:12:31 +0400

    ZIP: Reduced the loading screen's memory.

---
+++ ZIP's Big Adventure/Assets/Scripts/Other/LoadingText.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(UILabel))]
+public class LoadingText : MonoBehaviour {
+
+	public float speed = 0.2f;
+
+	IEnumerator Start(){
+
+		int ind = 0;
+		UILabel label = GetComponent<UILabel> ();
+
+		while(true){
+			yield return new WaitForSeconds(speed);
+			label.text = "LOADING" + (ind==0?"":ind==1||ind==5?".":ind==2||ind==4?"..":"...");
+			ind = ++ind%6;
+		}
+	}
+}
