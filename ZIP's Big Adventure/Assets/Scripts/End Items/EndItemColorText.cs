a25c745b83377d5e52bdf887d35bdd5e1bc51dff
Aharon Smbatyan <aharon91@mail.ru>
2014-07-29 13:43:39 -0800

    ZIP: Letter/Kindergarten/Match (Made the answers with different colors)

---
+++ ZIP's Big Adventure/Assets/Scripts/End Items/EndItemColorText.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[AddComponentMenu("EndItems/Color")]
+[RequireComponent(typeof(UILabel))]
+public class EndItemColorText : EndItem {
+
+	public Color to;
+	public float duration;
+	public float delay;
+
+	private bool saved=false;
+	private Color saveColor;
+	private UILabel sprite;
+	
+	public override void Reset ()
+	{
+		StopCoroutine ("Working");
+		if(!saved)
+		{
+			sprite = GetComponent<UILabel>();
+			saveColor = sprite.color;
+			saved = true;
+		}
+		sprite.color = saveColor;
+	}
+	
+	public override void Work ()
+	{
+		Reset ();
+		StartCoroutine ("Working");
+	}
+
+	IEnumerator Working()
+	{
+		yield return new WaitForSeconds (delay);
+		TweenColor.Begin (gameObject, duration, to);
+	}
+
+}
