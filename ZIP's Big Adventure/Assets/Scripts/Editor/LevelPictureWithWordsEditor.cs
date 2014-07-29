960b745d170067b826c26aaffda59deefc93d5fc
Aharon Smbatyan <aharon91@mail.ru>
2014-07-29 15:06:40 -0800

    ZIP: Letters/Kindergarten/GroupTheOpposites (Made the answers with different colors, fixed the placement)

---
+++ ZIP's Big Adventure/Assets/Scripts/Editor/LevelPictureWithWordsEditor.cs
@@ -0,0 +1,14 @@
+﻿using UnityEditor;
+using UnityEngine;
+using System.Collections;
+
+[CustomEditor(typeof(LevelPictureWithWords))]
+public class LevelPictureWithWordsEditor : Editor {
+	
+	public override void OnInspectorGUI() {
+		
+		base.OnInspectorGUI();
+		if(GUILayout.Button("Initialize"))
+			((LevelPictureWithWords)target).Initialize();
+	}
+}
--- ZIP's Big Adventure/Assets/Scripts/Levels/Match Picture With Words/LevelPictureWithWords.cs
+++ ZIP's Big Adventure/Assets/Scripts/Levels/Match Picture With Words/LevelPictureWithWords.cs
@@ -42,4 +42,41 @@ public class LevelPictureWithWords : ZIPLevel {
 		gameBlock.LevelComplete ();
 		
 	}
+
+	#region Editor
+
+	private Vector3[] c = new Vector3[] {
+		new Vector3(255,94,0),
+		new Vector3(0,120,199),
+		new Vector3(251,39,188),
+		new Vector3(62,120,199),
+		new Vector3(0,175,0),
+		new Vector3(219,0,191),
+		new Vector3(0,164,0),
+		new Vector3(255,130,0),
+		new Vector3(255,76,199),
+		new Vector3(116,76,199),
+		new Vector3(255,0,255),
+		new Vector3(210,77,202),
+		new Vector3(78,171,134)
+	};
+
+	private Color RandomColor(){
+		var cc = c [Random.Range (0, c.Length)];
+		Color d = new Color (cc.x / 256f, cc.y / 256f, cc.z / 256f, 1f);
+		return d;
+	}
+
+	public void Initialize(){
+		var p = transform.FindChild ("Game");
+		var p1 = p.transform.Find ("word1").GetComponent<UILabel> ();
+		var p2 = p.transform.Find ("word2").GetComponent<UILabel> ();
+		var p3 = p.transform.Find ("word3").GetComponent<UILabel> ();
+
+		p1.color = RandomColor ();
+		p2.color = RandomColor ();
+		p3.color = RandomColor ();
+	}
+	#endregion
+
 }
