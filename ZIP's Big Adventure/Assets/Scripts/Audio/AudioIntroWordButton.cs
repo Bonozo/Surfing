bcf927039186efb09d9a5489189abe48daee6f48
Aharon Smbatyan <aharon91@mail.ru>
2014-07-30 14:05:36 -0800

    ZIP: Letters/First/Jumbles (Added word VOs) ZIP: Letters/First/Punctuation (Added word VOs)

---
+++ ZIP's Big Adventure/Assets/Scripts/Audio/AudioIntroWordButton.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//[RequireComponent(typeof(BoxCollider))]
+public class AudioIntroWordButton : MonoBehaviour {
+
+	public AudioClip clip;
+	public float delay = 4f;
+	private bool ended = false;
+	IEnumerator Start(){
+		if(clip!=null){
+			yield return new WaitForSeconds(delay);
+			AudioManager.Instance.PlayClip(clip);
+			ended = true;
+		}
+	}
+
+	void OnClick(){
+		if(ended && clip!=null)
+			AudioManager.Instance.PlayClip(clip);
+	}
+}
