f3c449a3d1cebf5068e6c62eb7790246c5a2e0d4
Aharon Smbatyan <aharon91@mail.ru>
2014-09-11 02:45:58 -0800

    Mobi: Updated failed screens' continue button's color to green.

---
+++ MobisAdventure/Assets/_Upgrade/Scripts/Other/SetTextureColor.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SetTextureColor : MonoBehaviour {
+
+	public Texture2D texture;
+
+	void Start(){
+		for(int i=0;i<texture.width;i++)
+			for(int j=0;j<texture.height;j++){
+			var col = texture.GetPixel(i,j);
+			var c = col.r;
+			col.r = col.g;
+			col.g = c;
+			texture.SetPixel(i,j,col);
+		}
+
+		texture.Apply ();
+		byte[] bytes = texture.EncodeToPNG();
+		System.IO.File.WriteAllBytes("D:\\green.png", bytes);
+		
+	}
+}
